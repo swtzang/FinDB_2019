63fb7d9a35195ef99c977e3d587a2942543ceff2
swtzang <swtzang@gmail.com>
2019-06-20 22:21:42 +0800

    0620

--- TEJ_data/tej101.R
+++ TEJ_data/tej101.R
@@ -1,6 +1,17 @@
 library("Tejapi")
+library(tidyquant)
+
 Tejapi.api_key("1WpypSQZfNGk13As2EZ7kZlZgxud06")
-yourdata<- Tejapi('TWN/APRCD',coid='Y9999', mdate.gt='2018-01-01',mdate.lt='2018-02-01', paginate=TRUE)
+# yourdata<- Tejapi('TRAIL/TAPRCD', mdate.gt='2018-01-01',mdate.lt='2018-02-01', paginate=TRUE)
 
 yourdata <- Tejapi('TRAIL/TAPRCD', mdate.gt='2018-01-01',mdate.lt='2018-02-01',
+                   opts.columns=c('coid', 'mdate', 'close_d', 'mv', 'pmkt'),
+                   opts.sort='mdate.desc',
                    paginate=TRUE)
+yourdata.tbl <- yourdata %>% as_tibble()
+glimpse(yourdata.tbl)
+#
+data.tse <- yourdata.tbl %>% filter(pmkt == 'TSE')
+
+
+
